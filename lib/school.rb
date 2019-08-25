9050240f8b973f55718c2c5bcf96e0693c65e694
Mars Le <hp.mars.le@gmail.com>
2019-08-25 03:19:02 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,13 +1,14 @@
 class School
   
   attr_accessor :roster
-  def initialize(name)
-    @name = name
-    @roster = {}
-    
   
+  def initialize(school_name)
+    @school_name = school_name
+    @roster = {}
+
   end 
   
+  def add_student(student_name)
   # def roster=(roster)
   #   @roster = roster
   # end 
